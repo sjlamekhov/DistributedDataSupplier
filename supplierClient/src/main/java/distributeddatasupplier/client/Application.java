282a73320b7a70fd2b47acc75421de575c538b3b
Sergey Lamekhov <sergey.lamekhov@reltio.com>
2019-05-30 01:27:33 +0100

    initial commit

---
+++ supplierClient/src/main/java/distributeddatasupplier/client/Application.java
@@ -0,0 +1,8 @@
+package distributeddatasupplier.client;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("supplierClient started");
+        System.out.println("supplierClient finished");
+    }
+}
---
+++ supplierServer/src/main/java/distributeddatasupplier/server/Application.java
@@ -0,0 +1,8 @@
+package distributeddatasupplier.server;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("supplierServer started");
+        System.out.println("supplierServer finished");
+    }
+}
